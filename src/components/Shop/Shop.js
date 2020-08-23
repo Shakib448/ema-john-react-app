274934d4cc5f837398b4ca2370aa84a472c6ed38
Shakib448 <shakiba448@gmail.com>
2020-08-23 10:56:35 +0600

    ema-john-react-app

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,13 +2,18 @@ import React, {useState, useEffect} from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
+
     const [products, setProducts] = useState(first10);
 
+    const [cart, setCart] = useState([]);
+
     const handleAddProduct = (product) => {
-        console.log(product)
+        const newCart = [...cart, product]; // This product pass by clicked parameter
+        setCart(newCart);
     } 
     return (
         <div className="shop-container">
@@ -18,7 +23,7 @@ const Shop = () => {
                     }
             </div>
             <div className="cart-container">
-                <h3>This is cart</h3>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     )
