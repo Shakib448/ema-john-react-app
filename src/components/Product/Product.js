d7d6d77b63ac3e2400bfc3074e493e0d83ed4a35
Shakib448 <shakiba448@gmail.com>
2020-08-23 09:02:36 +0600

    ema-john-react-app

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 
@@ -16,7 +18,7 @@ const Product = (props) => {
                 <p><small>by :{seller} </small></p>
                 <p><small>$ :{price} </small></p>
                 <p><small>Only {stock} left in stock order soon </small></p>
-    
+                <button className="add-to-cart"> <FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
             </div>
         </div>
     )
