4da5bf03c2c04a78b0ef95e57bece42ffce5a3c6
Shakib448 <shakiba448@gmail.com>
2020-09-10 14:38:11 +0600

    ema john simple

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import './Shipment.css'
+import { UserContext } from '../../App';
+
+
 
 const Shipment = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+
+    console.log(watch("example")); // watch input value by passing the name of it
+
     return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
+        < form className="ship-form" onSubmit={handleSubmit(onSubmit)} >
+
+            < input name="name" defaultValue={loggedInUser.name} placeholder="Your name please" ref={register({ required: true })} />
+            {errors.name && <span className="error">Name is required</span>}
+
+            < input defaultValue={loggedInUser.email} name="email" ref={register({ required: true })} />
+            {errors.name && <span className="error">Email is required</span>}
+
+            < input placeholder="Your address" name="address" ref={register({ required: true })} />
+            {errors.name && <span className="error">Address is required</span>}
+
+            <input type="submit" />
+        </form >
     );
 }
 
